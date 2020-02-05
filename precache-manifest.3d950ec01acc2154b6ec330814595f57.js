ff9294ec343c6d9381b544ae56b841152ad7ded5
Gediminas Luksas <gediminasluksas@gmail.com>
2020-02-05 10:23:56 +0200

    Updates

---
+++ precache-manifest.3d950ec01acc2154b6ec330814595f57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e2a8819bb36347030fc1c4039d3e5fb",
+    "url": "/cv/static/media/img.8e2a8819.jpg"
+  },
+  {
+    "revision": "0a60133db753f53aac51",
+    "url": "/cv/static/js/runtime~main.0a60133d.js"
+  },
+  {
+    "revision": "6ebe44118a74ae49f38d",
+    "url": "/cv/static/js/main.6ebe4411.chunk.js"
+  },
+  {
+    "revision": "e613e366c38db8b1bd7d",
+    "url": "/cv/static/js/2.e613e366.chunk.js"
+  },
+  {
+    "revision": "6ebe44118a74ae49f38d",
+    "url": "/cv/static/css/main.152f6ee8.chunk.css"
+  },
+  {
+    "revision": "9c3622484e59000d4ba851427efd55c1",
+    "url": "/cv/index.html"
+  }
+];
